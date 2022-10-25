5c4246dae97d128e0af41d0e59fbdf51ec4452ad
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-10-26 01:04:11 +0300

    change env key retrieve method

--- backend/database/database.go
+++ backend/database/database.go
@@ -3,19 +3,19 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
-	"github.com/pektezol/leastportals/backend/controllers"
 )
 
 var DB *sql.DB
 
 func ConnectDB() {
-	host := controllers.GetEnvKey("DB_HOST")
-	port := controllers.GetEnvKey("DB_PORT")
-	user := controllers.GetEnvKey("DB_USER")
-	pass := controllers.GetEnvKey("DB_PASS")
-	name := controllers.GetEnvKey("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	name := os.Getenv("DB_NAME")
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, pass, name)
