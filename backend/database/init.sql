27feaa383a357b1a391c466295b0b8c3287599d3
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-10-29 00:37:39 +0300

    change default country_code to 'XX'

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   steam_id BIGINT,
   username VARCHAR(128) NOT NULL,
   avatar_link VARCHAR(128) NOT NULL,
-  country_code CHAR(2) NOT NULL DEFAULT 'X',
+  country_code CHAR(2) NOT NULL DEFAULT 'XX',
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP,
   user_type SMALLINT NOT NULL,
