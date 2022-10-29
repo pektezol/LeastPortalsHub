35de7615c7c67432aafa5f2f8c808e4a041059d1
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-10-29 17:58:58 +0300

    (#18) enums for user types

--- backend/models/models.go
+++ backend/models/models.go
@@ -3,11 +3,23 @@ package models
 import "time"
 
 type User struct {
-	SteamID     int64
+	SteamID     string
 	Username    string
 	AvatarLink  string
 	CountryCode string
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	UserType    int16
+}
+
+func (user *User) TypeToString() []string {
+	var list []string
+	switch user.UserType {
+	case 0:
+		list = append(list, "Normal")
+	}
+	if len(list) == 0 {
+		list = append(list, "Unknown")
+	}
+	return list
 }
