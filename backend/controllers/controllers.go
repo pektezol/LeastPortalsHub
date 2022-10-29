eede38cb4f2905c2c3568620fd78a4174091fcdf
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-10-30 01:24:43 +0300

    seperate user controller

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -4,13 +4,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/pektezol/leastportals/backend/database"
-	"github.com/pektezol/leastportals/backend/models"
 	"github.com/solovev/steam_go"
 )
 
@@ -87,73 +85,4 @@ func Logout(c *gin.Context) {
 			"output": "logout success",
 		})
 	}
-}
-
-func Profile(c *gin.Context) {
-	// Check if user exists
-	user, exists := c.Get("user")
-	if !exists {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"code": http.StatusUnauthorized,
-			"output": gin.H{
-				"error": "User not logged in. Could be invalid token.",
-			},
-		})
-	} else {
-		user := user.(models.User)
-		c.JSON(http.StatusOK, gin.H{
-			"code": http.StatusOK,
-			"output": gin.H{
-				"username": user.Username,
-				"avatar":   user.AvatarLink,
-				"types":    user.TypeToString(),
-			},
-			"profile": true,
-		})
-	}
-}
-
-func User(c *gin.Context) {
-	id := c.Param("id")
-	// Check if id is all numbers and 17 length
-	match, _ := regexp.MatchString("^[0-9]{17}$", id)
-	if !match {
-		c.JSON(http.StatusNotFound, gin.H{
-			"code": http.StatusNotFound,
-			"output": gin.H{
-				"error": "User not found.",
-			},
-		})
-		return
-	}
-	// Check if user exists
-	var targetUser models.User
-	database.DB.QueryRow(`SELECT * FROM users WHERE steam_id = $1;`, id).Scan(
-		&targetUser.SteamID, &targetUser.Username, &targetUser.AvatarLink, &targetUser.CountryCode,
-		&targetUser.CreatedAt, &targetUser.UpdatedAt, &targetUser.UserType)
-	if targetUser.SteamID == "" {
-		// User does not exist
-		c.JSON(http.StatusNotFound, gin.H{
-			"code": http.StatusNotFound,
-			"output": gin.H{
-				"error": "User not found.",
-			},
-		})
-		return
-	}
-	// Target user exists
-	_, exists := c.Get("user")
-	if exists {
-		c.Redirect(http.StatusFound, "/api/v1/profile")
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"code": http.StatusOK,
-		"output": gin.H{
-			"username": targetUser.Username,
-			"avatar":   targetUser.AvatarLink,
-			"types":    targetUser.TypeToString(),
-		},
-		"profile": false,
-	})
 }
