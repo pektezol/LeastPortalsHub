2c74aed740610ea1afaec70ca424e53907dd6389
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2023-02-03 00:44:22 +0300

    actually add update profile route (#30)

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -17,6 +17,7 @@ func InitRoutes(router *gin.Engine) {
 		v1.GET("/login", controllers.Login)
 		v1.GET("/profile", middleware.CheckAuth, controllers.Profile)
 		v1.PUT("/profile", middleware.CheckAuth, controllers.UpdateCountryCode)
+		v1.POST("/profile", middleware.CheckAuth, controllers.UpdateUser)
 		v1.GET("/user/:id", middleware.CheckAuth, controllers.FetchUser)
 		v1.GET("/demo", controllers.DownloadDemoWithID)
 		v1.GET("/maps/:id", middleware.CheckAuth, controllers.FetchMap)
