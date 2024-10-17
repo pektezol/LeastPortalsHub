dfae82694ff1f099d7af7ac7f9241c7a585f5d51
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2024-10-18 00:32:53 +0300

    backend: check map wr count by cm category

--- backend/handlers/user.go
+++ backend/handlers/user.go
@@ -294,7 +294,7 @@ func Profile(c *gin.Context) {
 	}
 	records := []ProfileRecords{}
 	// Get singleplayer records
-	sql = `SELECT sp.id, m.game_id, m.chapter_id, sp.map_id, m."name", (SELECT mh.score_count FROM map_history mh WHERE mh.map_id = sp.map_id ORDER BY mh.score_count ASC LIMIT 1) AS wr_count, sp.score_count, sp.score_time, sp.demo_id, sp.record_date
+	sql = `SELECT sp.id, m.game_id, m.chapter_id, sp.map_id, m."name", (SELECT mh.score_count FROM map_history mh WHERE mh.map_id = sp.map_id AND mh.category_id = 1 ORDER BY mh.score_count ASC LIMIT 1) AS wr_count, sp.score_count, sp.score_time, sp.demo_id, sp.record_date
 	FROM records_sp sp INNER JOIN maps m ON sp.map_id = m.id WHERE sp.user_id = $1 AND sp.is_deleted = false ORDER BY sp.map_id, sp.score_count, sp.score_time`
 	rows, err = database.DB.Query(sql, user.(models.User).SteamID)
 	if err != nil {
